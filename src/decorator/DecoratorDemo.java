77378bfb2ef2c9cbfdb47a37efd5d6b3e11b08c2
Yaloys <yanakyrpushko@gmail.com>
2025-04-28 11:50:39 +0300

    renaming

--- src/decorator/DecoratorDemo.java
+++ src/decorator/DecoratorDemo.java
@@ -3,19 +3,19 @@ import java.util.function.Function;
 
 public class DecoratorDemo {
     public static void main(String[] args) {
-        Function<String, String> diamond = (s) -> s + "Drawing Diamond\n";
+        Function<String, String> diamond = s -> s + "Diamond\n";
 
-        Function<String, String> addingGlow = (s) -> s + "Adding shadow\n";
-        Function<String, String> addingBorder = (s) -> s + "Adding glow\n";
+        Function<String, String> addingGlow = s -> s + "Adding glow\n";
+        Function<String, String> addingBorder = s -> s + "Adding border\n";
 
         Function<String, String> decoratedDiamond = diamond
                 .andThen(addingBorder)
                 .andThen(addingGlow);
 
-        System.out.println("Base shape:");
+        System.out.println("Drawing base shape:");
         System.out.print(diamond.apply(""));
 
-        System.out.println("\nDecorated shape with border and glow:");
+        System.out.println("\nDrawing decorated shape with border and glow:");
         System.out.print(decoratedDiamond.apply(""));
     }
 }
